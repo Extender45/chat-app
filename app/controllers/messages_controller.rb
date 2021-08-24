d390e07033681a6a76be0aff255e80b68cd3448e
Extender45 <86810892+Extender45@users.noreply.github.com>
2021-08-24 19:56:09 +0900

    最後の調整

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,16 +2,16 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @message = @room.messages.includes(:user)
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
-    @message = @room.message.new(message_params)
+    @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      @message = @room.message.includes(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
@@ -19,6 +19,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:messages).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
